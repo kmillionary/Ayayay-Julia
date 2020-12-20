3442ad66b1d15994acb376e2dd8be71b94146d6c
kmillionary <kmillionary@outlook.com>
2020-12-19 23:35:15 -0600

    commit 23

--- js/main.js
+++ js/main.js
@@ -1,56 +1,58 @@
-// Whatsapp button
+document.addEventListener("DOMContentLoaded", function() {
+    // Whatsapp button
+    var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?60159';
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = url;
+    var options = {
+        "enabled": true,
+        "chatButtonSetting": {
+            "backgroundColor": "#4dc247",
+            "ctaText": "",
+            "borderRadius": "25",
+            "marginLeft": "0",
+            "marginBottom": "20",
+            "marginRight": "20",
+            "position": "right"
+        },
+        "brandSetting": {
+            "brandName": "Ayayay Julia",
+            "brandSubTitle": "Papas horneadas y nachos.",
+            "brandImg": "../img/foto-perfil-fb.jpg",
+            "welcomeText": "Hola, ¿Cómo podemos ayudarte? 😁☕🍰",
+            "backgroundColor": "#0a5f54",
+            "ctaText": "Chatear ahora",
+            "borderRadius": "25",
+            "autoShow": false,
+            "phoneNumber": "50241566624"
+        }
+    };
+    s.onload = function() {
+        CreateWhatsappChatWidget(options);
+    };
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
 
-var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?60159';
-var s = document.createElement('script');
-s.type = 'text/javascript';
-s.async = true;
-s.src = url;
-var options = {
-    "enabled": true,
-    "chatButtonSetting": {
-        "backgroundColor": "#4dc247",
-        "ctaText": "",
-        "borderRadius": "25",
-        "marginLeft": "0",
-        "marginBottom": "20",
-        "marginRight": "20",
-        "position": "right"
-    },
-    "brandSetting": {
-        "brandName": "Ayayay Julia",
-        "brandSubTitle": "Papas horneadas y nachos.",
-        "brandImg": "../img/foto-perfil-fb.jpg",
-        "welcomeText": "Hola, ¿Cómo podemos ayudarte? 😁☕🍰",
-        "backgroundColor": "#0a5f54",
-        "ctaText": "Chatear ahora",
-        "borderRadius": "25",
-        "autoShow": false,
-        "phoneNumber": "50241566624"
+
+
+
+    scrollUp = document.getElementById("scrollUp");
+
+    window.onscroll = function() { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+            scrollUp.style.display = "block";
+        } else {
+            scrollUp.style.display = "none";
+        }
     }
-};
-s.onload = function() {
-    CreateWhatsappChatWidget(options);
-};
-var x = document.getElementsByTagName('script')[0];
-x.parentNode.insertBefore(s, x);
 
 
-
-
-scrollUp = document.getElementById("scrollUp");
-
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        scrollUp.style.display = "block";
-    } else {
-        scrollUp.style.display = "none";
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
     }
-}
 
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+});
